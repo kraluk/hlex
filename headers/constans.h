391d95818ed3d4b7ed64ed570f0e2ddb0c9f8c17
kraluk <kraluk@gmail.com>
2013-10-19 22:57:40 +0200

    Visual changes.

--- headers/constans.h
+++ headers/constans.h
@@ -9,19 +9,17 @@
 #define CONSTANS_H_
 
 
-const char* IDENTIFICATOR    = "identyfikator";
-const char* OPERATOR         = "operator";
-const char* NUMBER           = "liczba";
-
 const char* TAG_START        = "poczatek tagu";
 const char* TAG_STOP         = "koniec tagu";
 const char* TAG_NAME         = "nazwa tagu";
 const char* TAG_ENDING       = "tag koncowy";
+
 const char* PARAMETER        = "parametr";
 const char* PARAMETER_VALUE  = "wartosc parametru";
 const char* PARAMETER_SPACE  = "spacja";
 const char* PARAMETER_EQUALS = "znak rownosci";
 const char* PARAMETER_QUOTE  = "cudzyslow";
+
 const char* TEXT             = "tekst";
 
 
